3d3820faf91359f092bb584553239c3b67181063
Vansh <kvansh297@gmail.com>
2023-10-08 02:25:33 +0530

    fixed deployment

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,32 +1,47 @@
-"use client";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-// PDF actions
-// import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import packageJson from "../../package.json";
+// "use client";
+// import { Viewer, Worker } from "@react-pdf-viewer/core";
+// // PDF actions
+// // import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+// import packageJson from "../../package.json";
 
-const pdfjsVersion = packageJson.dependencies["pdfjs-dist"];
+// const pdfjsVersion = packageJson.dependencies["pdfjs-dist"];
 
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
+// import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
-type Props = {
-  pdf_url: string;
-};
+// type Props = {
+//   pdf_url: string;
+// };
+
+// const PDFViewer = ({ pdf_url }: Props) => {
+//   // const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+//   return (
+//     <Worker
+//       workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}
+//     >
+//       <div style={{ height: "750px" }}>
+//         <Viewer
+//           fileUrl={pdf_url}
+//           // plugins={[defaultLayoutPluginInstance]}
+//         />
+//       </div>
+//     </Worker>
+//   );
+// };
+
+// export default PDFViewer;
+
+import React from "react";
+
+type Props = { pdf_url: string };
 
 const PDFViewer = ({ pdf_url }: Props) => {
-  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
   return (
-    <Worker
-      workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}
-    >
-      <div style={{ height: "750px" }}>
-        <Viewer
-          fileUrl={pdf_url}
-          // plugins={[defaultLayoutPluginInstance]}
-        />
-      </div>
-    </Worker>
+    <iframe
+      src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
+      className="w-full h-full"
+    ></iframe>
   );
 };
 
