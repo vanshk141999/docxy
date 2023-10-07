2344912d24cd2c786a57182a112b34157c7e165b
Vansh <kvansh297@gmail.com>
2023-10-08 00:10:50 +0530

    fixed deployment

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -11,7 +11,6 @@ export const runtime = "edge";
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(config);
 
 export async function POST(req: Request) {
@@ -22,26 +21,25 @@ export async function POST(req: Request) {
       return NextResponse.json({ error: "chat not found" }, { status: 404 });
     }
     const fileKey = _chats[0].fileKey;
-
     const lastMessage = messages[messages.length - 1];
     const context = await getContext(lastMessage.content, fileKey);
 
     const prompt = {
       role: "system",
       content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
-        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-        AI is a well-behaved and well-mannered individual.
-        AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-        AI assistant is a big fan of Pinecone and Vercel.
-        START CONTEXT BLOCK
-        ${context}
-        END OF CONTEXT BLOCK
-        AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
-        If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-        AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
-        AI assistant will not invent anything that is not drawn directly from the context.
-        `,
+      The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+      AI is a well-behaved and well-mannered individual.
+      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+      AI assistant is a big fan of Pinecone and Vercel.
+      START CONTEXT BLOCK
+      ${context}
+      END OF CONTEXT BLOCK
+      AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+      If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+      AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+      AI assistant will not invent anything that is not drawn directly from the context.
+      `,
     };
 
     const response = await openai.createChatCompletion({
@@ -52,10 +50,9 @@ export async function POST(req: Request) {
       ],
       stream: true,
     });
-
     const stream = OpenAIStream(response, {
       onStart: async () => {
-        // Save user message into db
+        // save user message into db
         await db.insert(_messages).values({
           chatId,
           content: lastMessage.content,
@@ -63,7 +60,7 @@ export async function POST(req: Request) {
         });
       },
       onCompletion: async (completion) => {
-        // Save ai message into db
+        // save ai message into db
         await db.insert(_messages).values({
           chatId,
           content: completion,
@@ -71,7 +68,6 @@ export async function POST(req: Request) {
         });
       },
     });
-
     return new StreamingTextResponse(stream);
   } catch (error) {}
 }
